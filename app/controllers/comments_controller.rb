4271eab427d828ed6155e82bc7e3d8edd64a3c11
Eric Furrevig <efurrevig@gmail.com>
2023-01-06 02:36:50 -0500

    added delete comment button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @article
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body, :color)
